b469a8c4d0b0f8a2a354cc768be188a9740b5664
Ducemon <72726779+Ducemon@users.noreply.github.com>
2020-10-12 11:53:04 +0300

    Add files via upload

---
+++ TestInitial.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+using namespace std;
+struct prajitura
+{
+
+    int h;
+    float gem,frisca;
+    char denumire;
+
+struct cerc
+{
+    float raza;
+
+};
+
+struct dreptunghi
+{
+    float lM,lm;
+};
+struct patrat
+{
+    float l;
+};
+
+struct tr
+{
+    float cM,cm;
+
+};
+
+};
+
+void ADD(prajitura k)
+{
+
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    prajitura v[n+1];
+    while(n)
+    {
+
+    }
+}
